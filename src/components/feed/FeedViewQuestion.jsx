bf3eaf53dba57d06fba258c21b70077109d77b1a
rubin90 <rubin9008@gmail.com>
2023-03-29 21:05:41 +0900

    탐구 활동 증빙자료 퍼블리싱 추가

--- src/components/feed/FeedViewQuestion.jsx
+++ src/components/feed/FeedViewQuestion.jsx
@@ -203,7 +203,14 @@ function FeedViewQuestion({ feedId, feedDetail, refetchFeedDetail }) {
             </li>
           ))}
           {feedDetail?.activityYN === 'Y' && (
-            <button className="w-full py-2" onClick={() => setTab(4)}>탐구 활동 증빙 자료</button>
+            <li className="nav-item flex-1">
+              <button
+                className="nav-link w-full py-2"
+                onClick={() => setTab(4)}
+              >
+                탐구 활동 증빙 자료
+              </button>
+            </li>
           )}
         </ul>
         <div className="tab-content w-full border-l border-r border-b">
@@ -323,10 +330,229 @@ function FeedViewQuestion({ feedId, feedDetail, refetchFeedDetail }) {
             </div>
           ))}
           <div
-              className={`tab-pane leading-relaxed p-5 ${
-                tab === 4 && 'active'
-              }`}
-            >임과장 여기여</div>
+            className={`tab-pane leading-relaxed p-5 ${tab === 4 && 'active'}`}
+          >
+            {/* 1번 테이블 */}
+            <div className="intro-y grid grid-cols-12 gap-6 mt-5">
+              <div className="col-span-8">
+                <div className="flex justify-end">
+                  <button className="btn btn-green btn-sm">피드백</button>
+                </div>
+              </div>
+              <div className="col-span-4">
+                <div className="flex justify-end">
+                  <button className="btn btn-dark btn-sm">히스토리 보기</button>
+                </div>
+              </div>
+            </div>
+            <div className="intro-y grid grid-cols-12 gap-6">
+              <div className="col-span-8">
+                <table className="table table-bordered mt-3">
+                  <tbody>
+                    <tr>
+                      <td className="bg-slate-100">연번</td>
+                      <td className="bg-slate-100">증빙 자료 유형</td>
+                      <td className="bg-slate-100">제작 연월일</td>
+                      <td className="bg-slate-100  w-96">제목</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">1</td>
+                      <td>-</td>
+                      <td>2019년 5월 30일</td>
+                      <td>-</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">첨부파일</td>
+                      <td colSpan={4}>
+                        <div className="flex items-center">
+                          <div className="w-full">파일명: *.jpg</div>
+                          <div className="ml-auto">
+                            <button className="btn btn-primary w-20 btn-sm">
+                              다운로드
+                            </button>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={3} className="bg-slate-100">
+                        내용
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={4}>
+                        <p className="btn btn-sm btn-secondary">
+                          증빙 자료에 대한 요약설명(*띄어쓰기를 포함하여 200자
+                          이내)
+                        </p>
+                        <div className="mt-3">내용출력</div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={3}>
+                        <div className="flex items-center">
+                          <div className="btn mr-2">글자 수</div>
+                          <div className="mr-10">(0 / 200)</div>
+                          <div className="btn mr-2">최종 수정일</div>
+                          <div className="">23년 05 30일</div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-span-4">
+                <div className="bg-slate-100 p-5 rounded-md mt-3 outline_red relative">
+                  <div className="font-bold text-slate-500">피드백</div>
+                  <div className="mt-3">피드백 내용 출력</div>
+                  <button className="btn bg-white w-full btn-sm mt-3">
+                    수정
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* 2번 테이블 */}
+            <div className="intro-y grid grid-cols-12 gap-6 mt-5">
+              <div className="col-span-8">
+                <table className="table table-bordered mt-3">
+                  <tbody>
+                    <tr>
+                      <td className="bg-slate-100">연번</td>
+                      <td className="bg-slate-100">증빙 자료 유형</td>
+                      <td className="bg-slate-100">제작 연월일</td>
+                      <td className="bg-slate-100  w-96">제목</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">2</td>
+                      <td>-</td>
+                      <td>2019년 5월 30일</td>
+                      <td>-</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">첨부파일</td>
+                      <td colSpan={4}>
+                        <div className="flex items-center">
+                          <div className="w-full">파일명: *.jpg</div>
+                          <div className="ml-auto">
+                            <button className="btn btn-primary w-20 btn-sm">
+                              다운로드
+                            </button>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={3} className="bg-slate-100">
+                        내용
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={4}>
+                        <p className="btn btn-sm btn-secondary">
+                          증빙 자료에 대한 요약설명(*띄어쓰기를 포함하여 200자
+                          이내)
+                        </p>
+                        <div className="mt-3">내용출력</div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={3}>
+                        <div className="flex items-center">
+                          <div className="btn mr-2">글자 수</div>
+                          <div className="mr-10">(0 / 200)</div>
+                          <div className="btn mr-2">최종 수정일</div>
+                          <div className="">23년 05 30일</div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-span-4">
+                <div className="bg-slate-100 p-5 rounded-md mt-3 outline_red relative">
+                  <div className="font-bold text-slate-500">피드백</div>
+                  <div className="mt-3">피드백 내용 출력</div>
+                  <button className="btn bg-white w-full btn-sm mt-3">
+                    수정
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* 3번 테이블 */}
+            <div className="intro-y grid grid-cols-12 gap-6 mt-5">
+              <div className="col-span-8">
+                <table className="table table-bordered mt-3">
+                  <tbody>
+                    <tr>
+                      <td className="bg-slate-100">연번</td>
+                      <td className="bg-slate-100">증빙 자료 유형</td>
+                      <td className="bg-slate-100">제작 연월일</td>
+                      <td className="bg-slate-100  w-96">제목</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">3</td>
+                      <td>-</td>
+                      <td>2019년 5월 30일</td>
+                      <td>-</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-slate-100">첨부파일</td>
+                      <td colSpan={4}>
+                        <div className="flex items-center">
+                          <div className="w-full">파일명: *.jpg</div>
+                          <div className="ml-auto">
+                            <button className="btn btn-primary w-20 btn-sm">
+                              다운로드
+                            </button>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={3} className="bg-slate-100">
+                        내용
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={4}>
+                        <p className="btn btn-sm btn-secondary">
+                          증빙 자료에 대한 요약설명(*띄어쓰기를 포함하여 200자
+                          이내)
+                        </p>
+                        <div className="mt-3">내용출력</div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={3}>
+                        <div className="flex items-center">
+                          <div className="btn mr-2">글자 수</div>
+                          <div className="mr-10">(0 / 200)</div>
+                          <div className="btn mr-2">최종 수정일</div>
+                          <div className="">23년 05 30일</div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-span-4">
+                <div className="bg-slate-100 p-5 rounded-md mt-3 outline_red relative">
+                  <div className="font-bold text-slate-500">피드백</div>
+                  <div className="mt-3">피드백 내용 출력</div>
+                  <button
+                    className="btn bg-white w-full btn-sm mt-3"
+                    onClick={() => {
+                      setIsModal(true)
+                    }}
+                  >
+                    수정
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
