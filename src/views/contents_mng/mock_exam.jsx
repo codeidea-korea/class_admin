ae1d5341021dbffb66295c5d0fd4b9d3a504e154
rubin90 <rubin9008@gmail.com>
2023-04-25 21:16:49 +0900

    mock_exam 퍼블리싱 추가

--- src/views/contents_mng/mock_exam.jsx
+++ src/views/contents_mng/mock_exam.jsx
@@ -1,17 +1,20 @@
-import { Lucide, Modal, ModalBody } from "@/base-components";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Lucide, Modal, ModalHeader,ModalBody,ModalFooter,  Dropdown,DropdownToggle,DropdownMenu,DropdownContent,DropdownItem, } from '@/base-components'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function MockExam() {
   // 비디오 영상 모달
-  const [video, videoDetail] = useState(false);
+  const [video, videoDetail] = useState(false)
+
+  // 과목추가하기 모달
+  const [SubjectsAdd, SubjectsAddDetail] = useState(false);
 
   return (
     <>
       <div className="flex gap-2 mt-5">
         <button
           className="btn bg-white w-36"
-          onClick={() => alert("준비중입니다.")}
+          onClick={() => alert('준비중입니다.')}
         >
           영재원
         </button>
@@ -20,18 +23,51 @@ function MockExam() {
         </Link>
         <button
           className="btn bg-white w-36"
-          onClick={() => alert("준비중입니다.")}
+          onClick={() => alert('준비중입니다.')}
         >
           과학고
         </button>
       </div>
       <div className="intro-y box p-5 mt-5">
         <div className="flex">
-          <select name="" id="" className="form-select w-52">
+          {/* <select name="" id="" className="form-select w-52">
             <option value="">연도</option>
             <option value="2023">2023년</option>
             <option value="2022">2022년</option>
-          </select>
+          </select> */}
+
+          <div className="flex items-center gap-3">
+            <Dropdown>
+              <DropdownToggle>
+                <div className="input-group">
+                  <div
+                    id="input-group-email"
+                    className="input-group-text whitespace-nowrap"
+                  >
+                    과목
+                  </div>
+                  <input type="text" className="form-control" value={'화학'} />
+                </div>
+              </DropdownToggle>
+              <DropdownMenu className="w-40">
+                <DropdownContent>
+                  <DropdownItem>화학</DropdownItem>
+                  <DropdownItem>물리</DropdownItem>
+                </DropdownContent>
+              </DropdownMenu>
+            </Dropdown>
+
+            <button
+              className="btn btn-outline-primary border-dotted"
+              onClick={() => {
+                SubjectsAddDetail(true)
+              }}
+            >
+              <Lucide icon="Plus" className="w-4 h-4"></Lucide>
+            </button>
+
+          </div>
+
           <div className="flex ml-auto">
             <Link to="/mock_exam_form">
               <button className="btn btn-sky w-24">수정</button>
@@ -58,7 +94,7 @@ function MockExam() {
                 <button
                   className="btn btn-outline-primary flex items-center gap-2"
                   onClick={() => {
-                    videoDetail(true);
+                    videoDetail(true)
                   }}
                 >
                   <Lucide icon="Video" className="w-4 h-4"></Lucide>
@@ -85,7 +121,7 @@ function MockExam() {
                 <button
                   className="btn btn-outline-primary flex items-center gap-2"
                   onClick={() => {
-                    videoDetail(true);
+                    videoDetail(true)
                   }}
                 >
                   <Lucide icon="Video" className="w-4 h-4"></Lucide>
@@ -111,14 +147,14 @@ function MockExam() {
         backdrop=""
         show={video}
         onHidden={() => {
-          videoDetail(false);
+          videoDetail(false)
         }}
       >
         <ModalBody className="video_frame">
           <button
             className="video_x"
             onClick={() => {
-              videoDetail(false);
+              videoDetail(false)
             }}
           >
             <Lucide icon="X" className="w-8 h-8 text-white" />
@@ -133,8 +169,49 @@ function MockExam() {
         </ModalBody>
       </Modal>
       {/* END: Modal 영상보기 */}
+      
+      {/* BEGIN: Modal 과목추가하기 */}
+      <Modal
+        show={SubjectsAdd}
+        onHidden={() => {
+          SubjectsAddDetail(false);
+        }}
+      >
+        <ModalHeader>
+          <h2 className="font-medium text-base mr-auto">과목 추가하기</h2>
+          <button
+            className="btn btn-rounded-secondary hidden sm:flex p-1"
+            onClick={() => {
+              SubjectsAddDetail(false);
+            }}
+          >
+            <Lucide icon="X" className="w-4 h-4" />
+          </button>
+        </ModalHeader>
+        <ModalBody>
+          <div className="flex items-center">
+            <div className="w-16 shrink-0">과목</div>
+            <input type="text" className="form-control w-full" />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            type="button"
+            className="btn btn-ouline-secondary w-24 mr-2"
+            onClick={() => {
+              SubjectsAddDetail(false);
+            }}
+          >
+            취소
+          </button>
+          <button type="button" className="btn btn-sky w-24">
+            확인
+          </button>
+        </ModalFooter>
+      </Modal>
+      {/* END: Modal 과목추가하기 */}
     </>
-  );
+  )
 }
 
-export default MockExam;
+export default MockExam
