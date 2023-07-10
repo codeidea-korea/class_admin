26e55a5db136064408c5d8ecdcc84161df86931c
enan2 <friendjang@naver.com>
2023-07-10 17:57:46 +0900

    컨텐츠 관리 > 컨텐츠 URL 및 다운로드 메뉴 개발 - 영재원 테이블 코드 추가 - 영재원 관련 개발

--- src/views/contents_mng/contents.jsx
+++ src/views/contents_mng/contents.jsx
@@ -24,17 +24,25 @@ function Contents() {
   })
 
   const { isLoading: isGetMenus } = useQuery(
-    'getMenus',
+    ['getMenus', menu.mainMenu],
     () => request.get('/admin/content-management/menus'),
     {
       select: (data) => data.menuList,
       onSuccess: (data) => {
+        let result = {};
+
+        if(menu.mainMenu) {
+          result = data.filter(item => item?.ca_code1_name === menu?.mainMenu)[0];
+        }else {
+          result = data[0];
+        }
+
         setMenu({
-          mainMenu: data[0].ca_code1_name,
+          mainMenu: result.ca_code1_name,
           mainMenuList: data,
-          subMenu: data[0].subMenuResponseList[0].ca_code2_name,
-          subMenuList: data[0].subMenuResponseList,
-          code: data[0].subMenuResponseList[0].ca_code2,
+          subMenu: result.subMenuResponseList[0].ca_code2_name,
+          subMenuList: result.subMenuResponseList,
+          code: result.subMenuResponseList[0].ca_code2,
         })
       },
     },
@@ -122,7 +130,7 @@ function Contents() {
     formData.append('id', _.map(data, 'id').join(','))
     formData.append('link_url', _.map(data, 'link_url').join(','))
     formData.append('savedFileDelYN', newFileDelYN.join(','))
-    console.log(formData.get('id'))
+
     updateMenuDetail(formData)
   }
 
@@ -142,29 +150,34 @@ function Contents() {
         isUpdateMenuDetail) && <Loading />}
       <div className="flex items-center gap-3">
         <Dropdown>
-          <DropdownToggle className="btn bg-white">
-            {menu.mainMenu}
-            <Lucide icon="ChevronDown" className="w-4 h-4 ml-3"></Lucide>
-          </DropdownToggle>
-          <DropdownMenu className="w-40">
-            <DropdownContent>
-              {menu.mainMenuList?.map((item) => (
-                <DropdownItem
-                  key={item.ca_code1}
-                  className={`${
-                    menu.mainMenu === item.ca_code1_name && 'drop_active'
-                  }`}
-                  onClick={() => {
-                    setMenu({
-                      mainMenu: item.ca_code1_name,
-                    })
-                  }}
-                >
-                  {item.ca_code1_name}
-                </DropdownItem>
-              ))}
-            </DropdownContent>
-          </DropdownMenu>
+          {({ dismiss }) => (
+            <>
+              <DropdownToggle className="btn bg-white">
+                {menu.mainMenu}
+                <Lucide icon="ChevronDown" className="w-4 h-4 ml-3"></Lucide>
+              </DropdownToggle>
+              <DropdownMenu className="w-40">
+                <DropdownContent>
+                  {menu.mainMenuList?.map((item) => (
+                    <DropdownItem
+                      key={item.ca_code1}
+                      className={`${
+                        menu.mainMenu === item.ca_code1_name && 'drop_active'
+                      }`}
+                      onClick={() => {
+                        setMenu({
+                          mainMenu: item.ca_code1_name,
+                        })
+                        dismiss()
+                      }}
+                    >
+                      {item.ca_code1_name}
+                    </DropdownItem>
+                  ))}
+                </DropdownContent>
+              </DropdownMenu>
+            </>
+          )}
         </Dropdown>
 
         {menu.mainMenu && (
@@ -212,72 +225,72 @@ function Contents() {
         </div>
         <table className="table mt-5 table-bordered">
           <tbody>
-            <tr className="bg-slate-100 text-center">
-              <td>화면</td>
-              <td>버튼 명</td>
-              <td>링크/파일</td>
-            </tr>
-            {menu.menuDetailList.map((item) => (
-              <tr key={item.id}>
-                <td>{item.screen_name}</td>
-                <td>{item.btn_name}</td>
-                <td>
-                  {item.tf_file ? (
-                    item.fileName ? (
-                      <div className="flex items-center">
-                        <a
-                          href={`https://api.shuman.codeidea.io/v1/contents-data/file-download/${item.fileId}`}
-                          className="cursor-pointer text-blue underline"
-                        >
-                          {item.fileName}
-                        </a>
-                        <a
-                          className="text-danger ml-4 cursor-pointer"
-                          onClick={() => {
-                            setMenu({
-                              menuDetailList: menu.menuDetailList.map((child) =>
-                                child.id === item.id
-                                  ? {
-                                      ...child,
-                                      file: '',
-                                      tf_file: true,
-                                      fileName: false,
-                                      savedFileDelYN: 'Y',
-                                    }
-                                  : child,
-                              ),
-                            })
-                          }}
-                        >
-                          삭제
-                        </a>
-                      </div>
-                    ) : (
-                      <input
-                        type="file"
-                        className="form-control"
-                        onChange={(e) => handleChangeFile(e, item.id)}
-                      />
-                    )
+          <tr className="bg-slate-100 text-center">
+            <td>화면</td>
+            <td>버튼 명</td>
+            <td>링크/파일</td>
+          </tr>
+          {menu.menuDetailList.map((item) => (
+            <tr key={item.id}>
+              <td>{item.screen_name}</td>
+              <td>{item.btn_name}</td>
+              <td>
+                {item.tf_file ? (
+                  item.fileName ? (
+                    <div className="flex items-center">
+                      <a
+                        href={`https://api.shuman.codeidea.io/v1/contents-data/file-download/${item.fileId}`}
+                        className="cursor-pointer text-blue underline"
+                      >
+                        {item.fileName}
+                      </a>
+                      <a
+                        className="text-danger ml-4 cursor-pointer"
+                        onClick={() => {
+                          setMenu({
+                            menuDetailList: menu.menuDetailList.map((child) =>
+                              child.id === item.id
+                                ? {
+                                  ...child,
+                                  file: '',
+                                  tf_file: true,
+                                  fileName: false,
+                                  savedFileDelYN: 'Y',
+                                }
+                                : child,
+                            ),
+                          })
+                        }}
+                      >
+                        삭제
+                      </a>
+                    </div>
                   ) : (
                     <input
-                      type="text"
+                      type="file"
                       className="form-control"
-                      value={item.link_url}
-                      onChange={(e) =>
-                        setMenu({
-                          menuDetailList: menu.menuDetailList.map((child) =>
-                            child.id === item.id
-                              ? { ...child, link_url: e.target.value }
-                              : child,
-                          ),
-                        })
-                      }
+                      onChange={(e) => handleChangeFile(e, item.id)}
                     />
-                  )}
-                </td>
-              </tr>
-            ))}
+                  )
+                ) : (
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={item.link_url}
+                    onChange={(e) =>
+                      setMenu({
+                        menuDetailList: menu.menuDetailList.map((child) =>
+                          child.id === item.id
+                            ? { ...child, link_url: e.target.value }
+                            : child,
+                        ),
+                      })
+                    }
+                  />
+                )}
+              </td>
+            </tr>
+          ))}
           </tbody>
         </table>
       </div>
