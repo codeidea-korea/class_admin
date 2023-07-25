bd07a5821a727ea5a47b627953d5b33e2a996cd0
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2023-07-25 14:07:36 +0900

    상담예약 수정

--- src/views/reservation/ReservationList.jsx
+++ src/views/reservation/ReservationList.jsx
@@ -37,7 +37,7 @@ const ReservationList = () => {
 
   useEffect(() => {
     getMyReservationList();
-  }, [])
+  }, [pageParams.currentPage])
 
   const renderDataAsElements = (dataArray) => {
     return dataArray.map((item, index) => (
