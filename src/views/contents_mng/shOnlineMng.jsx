b85ebf9cc1bbdc6068d967198daaaaefe6c1ba90
reimex111 <reimex111@codeidea.dev>
2023-07-26 19:27:19 +0900

    영상학습 관리

--- src/views/contents_mng/shOnlineMng.jsx
+++ src/views/contents_mng/shOnlineMng.jsx
@@ -1,91 +1,124 @@
-import {useEffect, useReducer, useState} from 'react'
-import {
-    Lucide,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-  } from '@/base-components'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ReactPaginate from 'react-paginate'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useRecoilValue } from 'recoil'
+import { userState } from '@/states/userState'
+import useAxios from '@/hooks/useAxios'
 
-const ShOnlineMng = ()=>{
-    const [data,setData] = useState([
-        {rowId:1,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:2,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:3,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:4,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"}
-      ])
-      // 과목추가 모달
-      const [subject,setSubject] = useState(false)
-  
-      // 탭 이동
-      const [curTab,setCurTab] = useState("수학")
-  
-      // 페이지네이션 클릭
-      const handlePageClick = ()=>{}
+const ShOnlineMng = () => {
+  const [listLength, setListLength] = useState(0)
+  const api = useAxios()
+  const user = useRecoilValue(userState)
+  // const [data, setData] = useState([
+  //   { rowId: 1, title: '초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학', link: 'https://youtu.be/hwCFXw35ctc' },
+  //   { rowId: 2, title: '초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학', link: 'https://youtu.be/hwCFXw35ctc' },
+  //   { rowId: 3, title: '초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학', link: 'https://youtu.be/hwCFXw35ctc' },
+  //   { rowId: 4, title: '초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학', link: 'https://youtu.be/hwCFXw35ctc' },
+  // ])
+  const [data, setData] = useState([])
+  const [pageParams, setPageParams] = useState({
+    totalPages: 0, totalElements: 0, currentPage: 1, pageRangeDisplayed: 10,
+  })
+  // 과목추가 모달
+  const [subject, setSubject] = useState(false)
 
-    return (<>
-        <div className="flex gap-2 mt-5">
-          <button className={"btn w-32 " + (curTab === "수학"?"btn-primary":"bg-white")} onClick={()=>setCurTab("수학")}>수학</button>
-          <button className={"btn w-32 " + (curTab === "과학"?"btn-primary":"bg-white")}  onClick={()=>setCurTab("과학")}>과학</button>
-      </div>
-      <div className="intro-y box mt-5 relative">
-          <div className="p-5">
-              <div className="flex items-center gap-3">
-                  <div className="flex ml-auto gap-2">
-                      <Link to="/sh_online_mng/edit">
-                          <button className="btn btn-sky w-24">수정</button>
-                      </Link>
-                  </div>
-              </div>
-              <table className='table table-hover mt-5'>
-                  <thead>
-                      <tr className="bg-slate-100 text-center">
-                          <td>번호</td>
-                          <td>제목</td>
-                          <td>링크</td>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    {data.map((item,index)=>(
-                      <tr className="text-center" key={index}>
-                          <td>{item.rowId}</td>
-                          <td>{item.title}</td>
-                          <td><a href={item.link} target="_blank">{item.link}</a></td>
-                      </tr>
-                    ))}
-                  </tbody>
-              </table>
-              <div className="mt-5 flex items-center justify-center">
-              <div className="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center">
-                <nav className="w-full sm:w-auto sm:mr-auto">
-                  <ReactPaginate
-                    className={'pagination justify-center'}
-                    pageClassName={'page-item'}
-                    pageLinkClassName={'page-link'}
-                    breakLinkClassName={'page-item'}
-                    breakLabel="..."
-                    nextClassName={'page-item'}
-                    nextLinkClassName={'page-link'}
-                    nextLabel={<ChevronRight className="w-4 h-4"/>}
-                    previousClassName={'page-item'}
-                    previousLinkClassName={'page-link'}
-                    onPageChange={handlePageClick}
-                    previousLabel={<ChevronLeft className="w-4 h-4"/>}
-                    activeClassName={'page-item active'}
-                    // pageRangeDisplayed={pageParams.pageRangeDisplayed}
-                    pageRangeDisplayed = {"10"}
-                    // pageCount={pageParams.totalPages}
-                    pageCount={"3"}
-                    renderOnZeroPageCount={props => null}
-                  />
-                </nav>
-              </div>
-            </div>
+  // 탭 이동
+  const [curTab, setCurTab] = useState('MATH')
+
+  // 페이지네이션 클릭
+  const handlePageClick = () => {
+  }
+
+  const getDataList = () => {
+    api.get(`/admin/content-management/scienceOnlineClass?subjectType=${curTab}&page=${pageParams.currentPage}&limit=${pageParams.pageRangeDisplayed}`,
+      { headers: { Authorization: `Bearer ${user.token}` } })
+      .then((res) => {
+        if (res.status === 200) {
+          setData(res.data.content)
+          setPageParams({ ...pageParams, totalPages: res.data.totalPages, totalElements: res.data.totalElements })
+          setListLength(Number(pageParams.totalElements) - ((pageParams.currentPage - 1) * 10))
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getDataList()
+  }, [curTab])
+
+  return (<>
+    <div className='flex gap-2 mt-5'>
+      <button className={'btn w-32 ' + (curTab === 'MATH' ? 'btn-primary' : 'bg-white')}
+              onClick={() => {
+                setCurTab('MATH')
+              }}>수학
+      </button>
+      <button className={'btn w-32 ' + (curTab === 'SCIENCE' ? 'btn-primary' : 'bg-white')}
+              onClick={() => {
+                setCurTab('SCIENCE')
+              }}>과학
+      </button>
+    </div>
+    <div className='intro-y box mt-5 relative'>
+      <div className='p-5'>
+        <div className='flex items-center gap-3'>
+          <div className='flex ml-auto gap-2'>
+            <Link to={`/sh_online_mng/edit?curTab=${curTab}`}>
+              <button className='btn btn-sky w-24'>수정</button>
+            </Link>
           </div>
+        </div>
+        <table className='table table-hover mt-5'>
+          <thead>
+          <tr className='bg-slate-100 text-center'>
+            <td>번호</td>
+            <td>제목</td>
+            <td>링크</td>
+          </tr>
+          </thead>
+          <tbody>
+          {data?.length > 0 ? data?.map((item, index) => (
+              <tr className='text-center' key={index}>
+                <td>{listLength - index}</td>
+                <td>{item.title}</td>
+                <td><a href={item.link_url} target='_blank'>{item.link_url}</a></td>
+              </tr>
+            )) :
+            <tr className='text-center'>
+              <td colSpan={3}>데이터가 존재하지 않습니다.</td>
+            </tr>
+          }
+          </tbody>
+        </table>
+        <div className='mt-5 flex items-center justify-center'>
+          <div className='intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center'>
+            <nav className='w-full sm:w-auto sm:mr-auto'>
+              <ReactPaginate
+                className={'pagination justify-center'}
+                pageClassName={'page-item'}
+                pageLinkClassName={'page-link'}
+                breakLinkClassName={'page-item'}
+                breakLabel='...'
+                nextClassName={'page-item'}
+                nextLinkClassName={'page-link'}
+                nextLabel={<ChevronRight className='w-4 h-4' />}
+                previousClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                onPageChange={handlePageClick}
+                previousLabel={<ChevronLeft className='w-4 h-4' />}
+                activeClassName={'page-item active'}
+                pageRangeDisplayed={pageParams.pageRangeDisplayed}
+                pageCount={pageParams.totalPages}
+                renderOnZeroPageCount={props => null}
+              />
+            </nav>
+          </div>
+        </div>
       </div>
-    </>)
+    </div>
+  </>)
 }
-export default ShOnlineMng;
+export default ShOnlineMng
--- src/views/contents_mng/shOnlineMngEdit.jsx
+++ src/views/contents_mng/shOnlineMngEdit.jsx
@@ -1,102 +1,236 @@
 import { Lucide } from '@/base-components'
-import { Link } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 import React, { useState, useReducer } from 'react'
+import { useRecoilValue } from 'recoil'
+import { userState } from '@/states/userState'
+import { useForm } from 'react-hook-form'
+import { useMutation, useQuery } from 'react-query'
+import request from '@/utils/request'
 
-const ShOnlineMngEdit = ()=>{
-    const [data,setData] = useState([
-        {rowId:1,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:2,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:3,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"},
-        {rowId:4,title:"초등 수학 딱 대. 한번에 끝나는 초등 고학년 수학",link:"https://youtu.be/hwCFXw35ctc"}
+const ShOnlineMngEdit = () => {
+  const user = useRecoilValue(userState)
+  const userLocation = useLocation()
+  const queryParams = new URLSearchParams(userLocation.search)
+  const curTab = queryParams.get('curTab')
+  // const [idx, setIdx] = useState(1)
+  const [delDataList, setDelDataList] = useState([])
+  const { getValues, setValue, watch, reset, register } = useForm({
+    defaultValues: {
+      list: [],
+    },
+  })
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      list: [],
+    },
+  )
+
+  const {
+    data: basicClass,
+    isLoading: isBasicClassm,
+    refetch: refetchBasicClass,
+  } = useQuery(
+    'getBasicClass',
+    () => request.get(`/admin/content-management/scienceOnlineClass?subjectType=${curTab}&page=1&limit=9999`, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    }),
+    {
+      onSuccess: (data) => {
+        // setIdx(Number(idx) + res.data.content.length)
+        reset({ list: data.content })
+      },
+    },
+  )
+
+  // 저장
+  const { mutate: saveData } = useMutation(
+    (data) => request.put(`/admin/content-management/scienceOnlineClass`, data),
+    {
+      onSuccess: () => {
+        alert('저장되었습니다.')
+        location.reload()
+      },
+      onError: (e) => {
+        console.log(e)
+      },
+    },
+  )
+
+  // 삭제
+  const deleteHandle = (itemParam, index) => {
+    if (itemParam.row_id && itemParam.row_id > 0) {
+      setDelDataList([...delDataList, itemParam])
+    }
+    reset({
+      list: getValues('list').filter((_, i) => i !== index),
+    })
+  }
+
+  // 저장하기 버튼 클릭
+  const handleSave = async () => {
+    let rowIdList = getValues('list').map((item) => (item.row_id ? item.row_id : 0))
+    let titleList = getValues('list').map((item) => (item.title ? item.title : ''))
+    let linkUrlList = getValues('list').map((item) => (item.link_url ? item.link_url : ''))
+    let delYnList = getValues('list').map((item) => (item.delYN ? item.delYN : 'N'))
+
+    console.log(rowIdList)
+
+    delDataList.forEach((item) => {
+      rowIdList.push(item.row_id)
+      titleList.push(item.title)
+      linkUrlList.push(item.link_url)
+      delYnList.push('Y')
+    });
+
+    const formData = new FormData()
+    formData.append('row_id', rowIdList.join(','));
+    formData.append('title', titleList.join(','));
+    formData.append('link_url', linkUrlList.join(','));
+    formData.append('delYN', delYnList.join(','));
+
+    formData.append('totalCount', Number(getValues('list').length) + Number(delDataList.length))
+    formData.append('subjectType', curTab)
+
+    saveData(formData)
+  }
+
+  // + 버튼 클릭
+  const handleAddList = () => {
+    const newSchedule = {
+      row_id: 0,
+      title: null,
+      link_url: null,
+      delYN: 'N',
+    }
+    setValue('list', [
+      ...getValues('list'),
+      newSchedule,
     ])
-    console.log(data)
-    // + 버튼 클릭
-    const handleAddList = () => {
-        const addData = {rowId:data.length+1,title:"",link:""}
-        setData([...data,addData])
-    }
-    // 삭제
-    const deleteHandle = (rowId)=>{
-        const result = data.filter((item)=>item.rowId !== rowId)
-        setData(result)
-    }
+  }
 
-    return (<>
-        <div className="intro-y box mt-5">
-        <div className="p-3 px-5 flex items-center border-b border-slate-200/60">
-          <div className="text-lg font-medium flex items-center">
-            과학고 영상 학습 관리
-            <Lucide icon="ChevronRight" className="w-6 h-6 mx-3"></Lucide>
-            수학
-          </div>
+  // const saveData = () => {
+  //   let rowIdList = [];
+  //   let titleList = []
+  //   let linkUrlList = []
+  //   let delYnList = []
+  //
+  //   delDataList.forEach((item) => {
+  //     rowIdList.push(item.row_id)
+  //     titleList.push(item.title)
+  //     linkUrlList.push(item.link_url)
+  //     delYnList.push('Y')
+  //   })
+  //
+  //   data.forEach((item) => {
+  //     if (item.hasOwnProperty('row_id')) {
+  //       rowIdList.push(item.row_id)
+  //       titleList.push(item.title)
+  //       linkUrlList.push(item.link_url)
+  //       delYnList.push('N')
+  //     } else {
+  //       rowIdList.push(0)
+  //       titleList.push(item.title)
+  //       linkUrlList.push(item.link_url)
+  //       delYnList.push('N')
+  //     }
+  //   })
+  //
+  //   // api.put('/admin/content-management/scienceOnlineClass', {
+  //   //     row_id: rowIdList,
+  //   //     title: titleList,
+  //   //     link_url: linkUrlList,
+  //   //     delYN: delYnList,
+  //   //     totalCount: Number(delDataList.length) + Number(data.length),
+  //   //     subjectType: curTab,
+  //   //   },
+  //   //   { headers: { Authorization: `Bearer ${user.token}` } })
+  //   //   .then((res) => {
+  //   //     if (res.status === 200) {
+  //   //     }
+  //   //   })
+  //   //   .catch((err) => {
+  //   //     console.log(err)
+  //   //   })
+  // }
+
+  return (<>
+    <div className='intro-y box mt-5'>
+      <div className='p-3 px-5 flex items-center border-b border-slate-200/60'>
+        <div className='text-lg font-medium flex items-center'>
+          과학고 영상 학습 관리
+          <Lucide icon='ChevronRight' className='w-6 h-6 mx-3'></Lucide>
+          {curTab === 'MATH' ? '수학' : '과학'}
         </div>
-        <div className="intro-y p-5">
-          <table className="table table-hover">
-            <thead>
-              <tr className="bg-slate-100 text-center">
-                <td>번호</td>
-                <td>제목</td>
-                <td>링크</td>
-                <td>삭제</td>
-              </tr>
-            </thead>
-            <tbody>
-                {data.map((item,index)=>(
-                    <tr className="text-center" key={index}>
-                        <td>{index+1}</td>
-                        <td>
-                            <input
-                            type="text"
-                            className="form-control"
-                            defaultValue={item.title}
-                            key={item.title}
-                            />
-                        </td>
-                        <td>
-                            <input
-                            type="text"
-                            className="form-control"
-                            defaultValue={item.link}
-                            key={item.link}
-                            />
-                        </td>
-                        <td>
-                            {index > 0 && 
-                                <button 
-                                    className="btn btn-outline-danger bg-white btn-sm whitespace-nowrap"
-                                    onClick={()=>deleteHandle(item.rowId)}
-                                >
-                                    삭제
-                                </button>
-                            }
-                            
-                        </td>
-                    </tr>
-                ))}
-              <tr>
-                <td colSpan={5} className="text-center">
+      </div>
+      <div className='intro-y p-5'>
+        <table className='table table-hover'>
+          <thead>
+          <tr className='bg-slate-100 text-center'>
+            <td>번호</td>
+            <td>제목</td>
+            <td>링크</td>
+            <td>삭제</td>
+          </tr>
+          </thead>
+          <tbody>
+          {watch('list').map((item, index) => (
+            <tr className='text-center' key={`list-${index}`}>
+              <td>{index + 1}</td>
+              <td>
+                <input
+                  type='text'
+                  className='form-control'
+                  defaultValue={item.title}
+                  {...register(`list[${index}].title`)}
+                />
+              </td>
+              <td>
+                <input
+                  type='text'
+                  className='form-control'
+                  defaultValue={item.link_url}
+                  {...register(`list[${index}].link_url`)}
+                />
+              </td>
+              <td>
+                {index >= 0 &&
                   <button
-                    className="btn btn-outline-primary border-dotted"
-                    onClick={() => handleAddList()}
+                    className='btn btn-outline-danger bg-white btn-sm whitespace-nowrap'
+                    onClick={() => deleteHandle(item, index)}
                   >
-                    <Lucide icon="Plus" className="w-4 h-4"></Lucide>
+                    삭제
                   </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="flex mt-3 justify-center">
-            <div className="flex gap-2">
-              <Link to="/sh_online_mng">
-                <button className="btn bg-white w-24">취소</button>
-              </Link>
-              <button className="btn btn-sky w-24" >
-                저장하기
+                }
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan={5} className='text-center'>
+              <button
+                className='btn btn-outline-primary border-dotted'
+                onClick={() => handleAddList()}
+              >
+                <Lucide icon='Plus' className='w-4 h-4'></Lucide>
               </button>
-            </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <div className='flex mt-3 justify-center'>
+          <div className='flex gap-2'>
+            <Link to='/sh_online_mng'>
+              <button className='btn bg-white w-24'>취소</button>
+            </Link>
+            <button className='btn btn-sky w-24' onClick={() => handleSave()}>
+              저장하기
+            </button>
           </div>
         </div>
       </div>
-    </>)
+    </div>
+  </>)
 }
-export default ShOnlineMngEdit;
+export default ShOnlineMngEdit
